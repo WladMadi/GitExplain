f0d419f55307375bc12adc572c55425db226cffa
Wlad Madi <wlad@madi.space>
2024-12-05 20:17:14 +0300

    Initial Commit

---
+++ GitExplain/GitExplainApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitExplainApp.swift
+//  GitExplain
+//
+//  Created by Влад Мади on 05.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitExplainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
