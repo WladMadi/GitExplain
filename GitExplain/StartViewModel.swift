ad2e3cf6a2791d751953343a38c4cacfdde438a8
Wlad Madi <wlad@madi.space>
2024-12-05 20:54:34 +0300

    price

--- GitExplain/StartViewModel.swift
+++ GitExplain/StartViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 @Observable
 class StartViewModel {
     var user: UserModel
+    var price: Int = 600
     
     init() {
         self.user = .init(id: .init(), name: "Вася", pass: "12345")
